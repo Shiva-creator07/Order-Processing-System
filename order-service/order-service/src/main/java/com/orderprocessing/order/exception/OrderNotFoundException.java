9267c80de97e0ed934f4df1d0e198b362ecc9995
Your Name <your@email.com>
2026-07-27 12:57:08 +0530

    Your message

--- order-service/order-service/src/main/java/com/orderprocessing/order/exception/OrderNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.orderprocessing.order.exception;
-
-import java.util.UUID;
-
-public class OrderNotFoundException extends RuntimeException {
-    public OrderNotFoundException(UUID orderId) {
-        super("Order not found: " + orderId);
-    }
-}
--- order-service/order-service/src/main/java/com/orderprocessing/order/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.orderprocessing.order.repository;
-
-import com.orderprocessing.order.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrderRepository extends JpaRepository<Order, UUID> {
-}
